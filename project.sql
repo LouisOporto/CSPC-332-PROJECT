b1f790f71f0fe1eef6f4b9edb7e2fe472a87524f
LouisOporto <louisoporto042@csu.fullerton.edu>
2024-11-14 00:52:14 -0800

    Adjusted some parmeters of elements to be more accurate
    Included foreign keys between tables
    
    Questions on what types are best for some variables but mostly settled

--- project.sql
+++ project.sql
@@ -2,10 +2,10 @@ CREATE DATABASE sysdb;
 
 CREATE TABLE PROFESSOR (
 	SSN numeric(9) primary key,
-    NAME varchar(40),
-    STATE varchar(20),
+    NAME varchar(20),
+    STATE char(2),
     CITY varchar(20),
-    STRADDRESS varchar(20),
+    STREETADDRESS varchar(20),
     ZIPCODE char(5),
     SEX enum('M','F'),
     SALARY numeric(5),
@@ -15,57 +15,68 @@ CREATE TABLE PROFESSOR (
 );
 
 CREATE TABLE SECTIONS (
-	SNUM numeric(3) primary key,
+	SNUM char(7) primary key,
     NUMSEATS numeric(2),
     BEGINTIME timestamp,
     ENDTIME timestamp,
-    CLASSROOM numeric(3)
-);
-
-CREATE TABLE RECORDS (
-	SNUM numeric(3),
-    CWID numeric(8),
-    Grade char(1)
-);
-
-CREATE TABLE COURSE (
-	CNUM numeric(3) primay key,
-    TITLE varchar(20),
-    UNITS numeric(2),
-    TEXTBOOK varchar(20)
-);
-
-CREATE TABLE STUDENT (
-	CWID numeric(9) primay key,
-    FNAME varchar(20),
-    LNAME varchar(20),
-    PHONENUMBER varchar(10),
-    ADDRESS varchar(20),
-    MAJDEPTNUM numeric(2)
-);
-
-CREATE TABLE DEPARTMENT (
-	DNUM numeric(2) primary key,
-    NAME varchar(20),
-    TELEPHONE numeric(10),
-    OFFICELOCATION varchar(20),
+    CLASSROOM numeric(3),
+    COURSENUM char(7),
     PROFSSN numeric(9)
 );
 
-CREATE TABLE MINORDEPARTMENT (
-	STUDENTCWID numeric(9) primary key,
-    MINDEPTNUM numeric(2)
+CREATE TABLE RECORDS (
+	SNUM char(7),
+    CWID numeric(9),
+    GRADE enum('A', 'B', 'C', 'D', 'F', 'W'),
+    primary key(SNUM, CWID)
 );
 
-CREATE TABLE COLLEGEDEGREES (
+CREATE TABLE COURSE (
+	CNUM char(7) primary key,
+    TITLE varchar(20),
+    UNITS tinyint,
+    TEXTBOOK varchar(50),
+    DNUM numeric(4)
+);
+
+CREATE TABLE STUDENT (
+	CWID numeric(9) primary key,
+    FNAME varchar(20),
+    LNAME varchar(20),
+    PHONENUMBER numeric(10),
+    ADDRESS varchar(20),
+    MAJORDEPTNUM numeric(4)
+);
+
+CREATE TABLE DEPARTMENT (
+	DNUM numeric(4) primary key,
+    NAME varchar(20),
+    TELEPHONE numeric(10),
+    OFFICELOCATION varchar(20),
+    CHAIRPERSONSSN numeric(9)
+);
+
+CREATE TABLE MINOR_IN (
+	STUDENTCWID numeric(4) primary key,
+    MINORDEPTNUM numeric(4)
+);
+
+CREATE TABLE COLLEGE_DEGREES (
 	PROFSSN numeric(9) primary key,
     DEGREE varchar(20)
 );
 
-CREATE TABLE MEETINGDAYS (
-	SNUM numeric(3) primary key,
+CREATE TABLE MEETING_DAYS (
+	SNUM numeric(7) primary key,
     MEETINGDATE time
 );
+
+CREATE TABLE PREQUISITE_COURSES (
+	CNUM numeric(7) primary key,
+    PREREQNUM numeric(7)
+);
+
+SHOW TABLES;
 
 
 
