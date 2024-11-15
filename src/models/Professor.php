44125415382af774497db41ac94486ec1e0bf105
Ethan Kim <ekim1098@csu.fullerton.edu>
2024-11-14 17:45:23 -0800

    Retrieves professor info given their SSN.

--- src/models/Professor.php
+++ src/models/Professor.php
@@ -6,13 +6,22 @@ class Professor {
       $this->db = $db;
     }
 
-    // Retrieve a professor's class schedule via SSN.
+    // Given the SSN of a professor, retrieves titles, classrooms, meeting times, and times of his/her classes.
     public function getClassSchedule($ssn) {
+      // Prepare SQL query.
       $stmt = $this->db->prepare("
         SELECT SECTIONS.SNUM, SECTIONS.CLASSROOM, SECTIONS.BEGINTIME, SECTIONS.ENDTIME, SECTIONS.MEETING_DAYS, COURSE.TITLE
         FROM SECTIONS
         JOIN COURSE ON SECTIONS.COURSENUM = COURSE.CNUM
         WHERE SECTIONS.PROFSSN = ?
-        ")
+        ");
+        // Prepare and bind parameters.
+        $stmt->bind_param("s", $ssn);
+        // Set parameters and execute.
+        $stmt->execute();
+        // Get result set.
+        $result = $stmt->get_result();
+        // Get all results and associate them.
+        return $result->fetch_all(MYSQLI_ASSOC);
     }
 }
