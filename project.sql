e36ae21aa1bab5ee8fd5f14e195f70ae7427c3c4
LouisOporto <louisoporto042@csu.fullerton.edu>
2024-11-14 16:40:39 -0800

    Reformat elements to be lowercase \n Added some funtions below creaion functions

--- project.sql
+++ project.sql
@@ -1,82 +1,110 @@
 CREATE DATABASE sysdb;
 
 CREATE TABLE PROFESSOR (
-	SSN numeric(9) primary key,
-    NAME varchar(20),
-    STATE char(2),
-    CITY varchar(20),
-    STREETADDRESS varchar(20),
-    ZIPCODE char(5),
-    SEX enum('M','F'),
-    SALARY numeric(5),
-    TITLE varchar(20),
-    AREACODE numeric(3),
-    PHONENUMBER numeric(10)
+	ssn numeric(9) primary key,
+    name varchar(20),
+    state char(2),
+    city varchar(20),
+    streetaddress varchar(20),
+    zipcode char(5),
+    sex enum('M','F'),
+    salary numeric(5),
+    title varchar(20),
+    areacode numeric(3),
+    phonenumber numeric(10)
 );
 
 CREATE TABLE SECTIONS (
-	SNUM char(7) primary key,
-    NUMSEATS numeric(2),
-    BEGINTIME timestamp,
-    ENDTIME timestamp,
-    CLASSROOM numeric(3),
-    COURSENUM char(7),
-    PROFSSN numeric(9)
+	snum char(7) primary key,
+    numseats numeric(2),
+    begintime timestamp,
+    endtime timestamp,
+    classroom numeric(3),
+    coursenum char(7),
+    profssn numeric(9)
 );
 
 CREATE TABLE RECORDS (
-	SNUM char(7),
-    CWID numeric(9),
-    GRADE char(2),
-    primary key(SNUM, CWID)
+	snum char(7),
+    cwid numeric(9),
+    grade char(2),
+    primary key(snum, cwid)
 );
 
 CREATE TABLE COURSE (
-	CNUM char(7) primary key,
-    TITLE varchar(20),
-    UNITS tinyint,
-    TEXTBOOK varchar(50),
-    DNUM numeric(4)
+	cnum char(7) primary key,
+    title varchar(20),
+    units tinyint,
+    textbook varchar(50),
+    dnum numeric(4)
 );
 
 CREATE TABLE STUDENT (
-	CWID numeric(9) primary key,
-    FNAME varchar(20),
-    LNAME varchar(20),
-    PHONENUMBER numeric(10),
-    ADDRESS varchar(20),
-    MAJORDEPTNUM numeric(4)
+	cwid numeric(9) primary key,
+    fname varchar(20),
+    lname varchar(20),
+    phonenumber numeric(10),
+    address varchar(20),
+    majordeptnum numeric(4)
 );
 
 CREATE TABLE DEPARTMENT (
-	DNUM numeric(4) primary key,
-    NAME varchar(20),
-    TELEPHONE numeric(10),
-    OFFICELOCATION varchar(20),
-    CHAIRPERSONSSN numeric(9)
+	dnum numeric(4) primary key,
+    name varchar(20),
+    phonenumber numeric(10),
+    officelocation varchar(20),
+    chairpersonssn numeric(9)
 );
 
 CREATE TABLE MINOR_IN (
-	STUDENTCWID numeric(4) primary key,
-    MINORDEPTNUM numeric(4)
+	studentcwid numeric(4) primary key,
+    minordeptnum numeric(4)
 );
 
 CREATE TABLE COLLEGE_DEGREES (
-	PROFSSN numeric(9) primary key,
-    DEGREE varchar(20)
+  	profssn numeric(9) primary key,
+    degree varchar(20)
 );
 
 CREATE TABLE MEETING_DAYS (
-	SNUM numeric(7) primary key,
-    MEETINGDATE time
+	snum numeric(7) primary key,
+    meetingdate time
 );
 
 CREATE TABLE PREQUISITE_COURSES (
-	CNUM numeric(7) primary key,
-    PREREQNUM numeric(7)
+	cnum numeric(7) primary key,
+    prereqnum numeric(7)
 );
 
+-- Insert values into tables here --
+
+INSERT INTO PROFESSOR VALUES (
+	123456789,
+    'Wang',
+    'CA',
+    'Fullerton',
+    'Robin',
+    '90420',
+    'M',
+    70000,
+    'Teacher',
+    1,
+    5621231234
+    );
+    
+    
+    
+    
+-- Show tables --
 SHOW TABLES;
+
+SELECT *
+FROM PROFESSOR;
+
+
+-- Quick Remove certain tabs
+
+DROP DATABASE sysdb;
 
 
 
