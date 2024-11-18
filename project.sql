d209becc147a343cea245f5a1768284ffd42d68f
LouisOporto <louisoporto042@csu.fullerton.edu>
2024-11-18 15:33:40 -0800

    Added query in .sql

--- project.sql
+++ project.sql
@@ -120,15 +120,20 @@ SHOW TABLES;
 SELECT *
 FROM PROFESSOR;
 
+-- QUERY --
+
+-- Given the social security of a professor, list the titles, classrooms, meeting days and time of his/her classes.
 SELECT SECTIONS.snum, SECTIONS.classroom, SECTIONS.begintime, SECTIONS.endtime, meetingdate
 FROM SECTIONS
-INNER JOIN MEETING_DAYS MD ON MD.snum = SECTIONS.snum
--- WHERE COURSE.cnum = SECTIONS.coursenum AND SECTIONS.profssn = 123456789;
+INNER JOIN MEETING_DAYS MD ON MD.snum = SECTIONS.snum;
 
-JOIN MEETING_DAYS ON SECTIONS.snum = MEETING_DAYS.snum
-WHERE SECTIONS.coursenum = COURSE.cnum AND MEETING_DAYS.snum = SECTIONS.snum AND SECTIONS.profssn = 123456789;
-GROUP BY SECTIONS.snum;
+-- Given a course number and a section number, count how many students  get each distinct, i.e. 'A', 'A-', 'B+', 'B', 'B', etc.->
+SELECT grade, COUNT(*) AS grade_count
+FROM RECORDS, COURSE
+WHERE SNUM = ? AND COURSE_NUM = ?
+GROUP BY grade;
+
+
 
 -- Quick Remove certain tabs
-
 DROP DATABASE sysdb;
