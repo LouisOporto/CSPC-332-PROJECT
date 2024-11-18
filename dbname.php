fae36ec1e571ccb54cf4a58f0590d42171f22ad6
LouisOporto <louisoporto042@csu.fullerton.edu>
2024-11-18 14:56:26 -0800

    Added test function of Professor.php

--- dbname.php
+++ dbname.php
@@ -2,11 +2,15 @@
   <body>
     <?php
 
+    // if($_POST["ssn"]) {
+    //     $ssn = $_POST["ssn"];
+    // }
+    $ssn = "123456789";
     // username and password need to be replaced by your username and password
-    $hostname = "localhost";
+    $hostname = "localhost"; // Server on-campus is "an ip not sure"
     $username = "root";
-    $password = "password";
-    $dbname = "sysdb";
+    $password = "";
+    $dbname = "sysdb"; // Server on-campus is "mariadb"
 
     // Create connection
     $link = mysqli_connect($hostname, $username, $password, $dbname);
@@ -18,12 +22,21 @@
 
 
     // Test query
-    $query = "SELECT * FROM PROFESSOR WHERE ssn=" .$_POST["ssn"];
+    $query = "SELECT * FROM PROFESSOR WHERE ssn=" .$ssn;
     $result = $link->query($query);
     $row = $result->fetch_assoc();
     printf("SSN: %s<br>\n", $row["ssn"]);
     printf("NAME: %s<br>\n", $row["name"]);
     $result->free_result();
+    
+    include("src/models/Professor.php");
+    
+    $professor = new Professor($link);
+    
+    $result = $professor->getClassSchedule($ssn);
+    printf("SNUM: %s\n", $result[0]["snum"]);
+    printf("MEETING DATE: %s\n", $result[0]["meetingdate"]);
+
     $link->close();
     ?>
   </body>
