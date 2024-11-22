f276c9b931047e7d4d0f501d396006a89d8ca752
LouisOporto <louisoporto042@csu.fullerton.edu>
2024-11-22 15:10:38 -0800

    Fix some data

--- data.sql
+++ data.sql
@@ -13,10 +13,20 @@ INSERT INTO PROFESSOR VALUES (
     1,
     5621231234
     );
-
-INSERT INTO STUDENT VALUES (
-	123456887
-);
+    
+INSERT INTO PROFESSOR VALUES (
+	123456788,
+    'Williams',
+    'CA',
+    'Los Angeles',
+    'Sparrow',
+     90731,
+     'M',
+    2000.00,
+    'Teacher',
+    1,
+    5621233456
+    );
 
 INSERT INTO SECTIONS VALUES (
 	47389,
@@ -28,6 +38,16 @@ INSERT INTO SECTIONS VALUES (
     123456789
 );
 
+INSERT INTO SECTIONS VALUES (
+	47388,
+    20,
+    '01:00:00',
+    '02:15:00',
+    484,
+    'CPSC484',
+    123456788
+);
+
 INSERT INTO COURSE VALUES (
 	'CPSC332',
     'Database Structure',
@@ -36,31 +56,62 @@ INSERT INTO COURSE VALUES (
     'CPSC'
 );
 
-INSERT INTO MEETING_DAYS VALUES (
-	47389,
-    '12:10:10'
+INSERT INTO COURSE VALUES (
+	'CPSC484',
+    'Graphics Principles',
+    3,
+    '3d rendering book',
+    'CPSC'
+);
+
+INSERT INTO STUDENT VALUES (
+	123456788,
+    'John',
+    'Smith',
+    5623214545,
+    'fullerton',
+    'CSPC'
+);
+
+INSERT INTO STUDENT VALUES (
+	123456889,
+    'John',
+    'Second',
+    5623215656,
+    'fullerton',
+    'CSPC'
 );
 
 INSERT INTO MEETING_DAYS VALUES (
 	47389,
-    '10:10:10'
+    '2024-10-10'
+);
+
+INSERT INTO MEETING_DAYS VALUES (
+	47388,
+    '2024-10-10'
+);
+
+INSERT INTO MEETING_DAYS VALUES (
+	47389,
+    '2024-10-11'
 );
 
 INSERT INTO RECORDS VALUES (
 	47389,
-	12345689,
+	123456889,
     'A+'
 );
 
 INSERT INTO RECORDS VALUES (
 	47389,
-	123456887,
+	123456788,
     'A+'
 );
 
 INSERT INTO RECORDS VALUES (
 	47388,
-    1234567887,
+    123456788,
     'B'
 );
 
